c20c11decd12da8f997b0e63eaa1d7b390b30418
JS <jeremystephens2@gmail.com>
2018-02-19 18:09:45 +0100

    created userfixture

--- src/Migrations/Version20180219162438.php
+++ src/Migrations/Version20180219162438.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180219150100 extends AbstractMigration
+class Version20180219162438 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
